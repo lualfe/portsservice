208392734b3a6184f93122b4bbb50c0e9721c8ea
lualfe <lucas.albuquerque.felipini@gmail.com>
2022-12-01 18:12:53 -0300

    chore: implemented application entrypoint

--- cmd/portsservice/main.go
+++ cmd/portsservice/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
 
+	"github.com/lualfe/portsservice/internal/infrastructure/repository"
+	"github.com/lualfe/portsservice/internal/usecase"
+)
+
+func main() {
+	file := flag.String("filename", "", "the path and name of the file containing the ports")
+	flag.Parse()
+
+	db := repository.NewInMemory()
+
+	portsUseCase := usecase.NewPorts(db)
+
+	ctx := context.Background()
+
+	if err := portsUseCase.Save(ctx, *file); err != nil {
+		fmt.Println(err)
+	}
 }
