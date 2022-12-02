c52fc52be713e03a8c2daad450098767f1f98219
lualfe <lucas.albuquerque.felipini@gmail.com>
2022-12-01 21:58:18 -0300

    refactor: migrated repository to redis

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/lualfe/portsservice
 go 1.19
 
 require github.com/golang/mock v1.6.0
+
+require (
+	github.com/Netflix/go-env v0.0.0-20220526054621-78278af1949d // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v9 v9.0.0-rc.2 // indirect
+)
--- internal/infrastructure/repository/repository.go
+++ internal/infrastructure/repository/repository.go
@@ -2,20 +2,37 @@ package repository
 
 import (
 	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/go-redis/redis/v9"
 
 	"github.com/lualfe/portsservice/internal/entity"
 )
 
 // InMemory database
-type InMemory map[string]entity.Port
+type InMemory struct {
+	client redisClient
+}
+
+type redisClient interface {
+	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) *redis.StatusCmd
+}
 
 // NewInMemory starts a new InMemory database.
-func NewInMemory() InMemory {
-	return make(InMemory)
+func NewInMemory(address, password string) *InMemory {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     address,
+		Password: password,
+	})
+	return &InMemory{
+		client: rdb,
+	}
 }
 
 // UpsertPort inserts a port and updates it if it already exists.
-func (i InMemory) UpsertPort(ctx context.Context, port entity.Port) error {
-	i[port.Key] = port
-	return nil
+func (i *InMemory) UpsertPort(ctx context.Context, port entity.Port) error {
+	data, _ := json.Marshal(port)
+	cmd := i.client.Set(ctx, port.Key, string(data), 0)
+	return cmd.Err()
 }
